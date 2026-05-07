5cc3a5512f8893795c435eda171b3cf30aa574e8
Varsedile <vansh.khetarpal21@gmail.com>
2026-05-07 12:33:34 +0530

    Updated manifest.json

--- src/content.js
+++ src/content.js
@@ -9,14 +9,35 @@ let selector = '';
 
 function buildSelector() {
   const parts = [];
-  if (settings.skipIntro) {
-    parts.push('[data-uia="player-skip-intro"]');
+
+  if (window.location.href.includes("netflix.com")) {
+    if (settings.skipIntro) {
+      parts.push('[data-uia="player-skip-intro"]');
+    }
+    if (settings.skipRecap) {
+      parts.push('[data-uia="player-skip-recap"]');
+    }
+    if (settings.skipCredits) {
+      parts.push('.skip-credits a', '.watch-video--skip-content-button');
+    }
   }
-  if (settings.skipRecap) {
-    parts.push('[data-uia="player-skip-recap"]');
-  }
-  if (settings.skipCredits) {
-    parts.push('.skip-credits a', '.watch-video--skip-content-button');
+  if (window.location.href.includes("hotstar.com")) {
+    if (settings.skipIntro) {
+      var allButtons = document.getElementsByTagName(button);
+      for(var i=0;i<mySpans.length;i++){
+      if(mySpans[i].innerHTML == 'Skip Intro'){
+      var parent = mySpans[i].parentNode;
+      break;
+      }}
+      console.log(parent)
+      parts.push('[class="_1CSTLo7uotP5mTlp3jKun7 relative overflow-hidden flex flex-row justify-center items-center RADIUS_03 py-SPACE_04 px-SPACE_05 px-SPACE_08 py-SPACE_04 rounded RADIUS_03 _1hvr__NMJbQHNb3s6iYa1w BODY2_MEDIUM TEXT_COLOR_L0"]');
+    }
+    if (settings.skipRecap) {
+      parts.push('[data-uia="player-skip-recap"]');
+    }
+    if (settings.skipCredits) {
+      parts.push('.skip-credits a', '.watch-video--skip-content-button');
+    }
   }
   selector = parts.join(', ');
 }
